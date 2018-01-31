07c1f636d1440a170c440dcaa7aa331c79026bf4
MOHAMMAD RIZQI HAFIYYANDI - <mohammadrizqihafiyyandi-@172-16-240-178.DYNAPOOL.NYU.EDU>
2018-01-31 14:53:47 -0500

    added count functionality

--- week2/server.js
+++ week2/server.js
@@ -1,18 +1,23 @@
-//app.use(express.static('public'));
-
 var express = require('express');
 var app = express();
 
+var count=0;
+
 app.use(express.static('public'));
 
+app.get('/count', function (req, res) {
+  count++;
+  res.send("<html><body><h1>+"count+"</h1></body></html>")
+});
+
 app.get('/', function (req, res) {
-  res.send('HEY')
+  res.send('HEY');
+});
+
+app.get('/somethingelse', function (req, res) {
+  res.send('AYE YO');
 });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-});
-
-app.get('/somethingelse', function (req, res) {
-  res.send('AYE YO')
 });
