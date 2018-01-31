436ea21b66ee62366be48e00b13c4d0e06f07456
MOHAMMAD RIZQI HAFIYYANDI - <mohammadrizqihafiyyandi-@172-16-240-178.DYNAPOOL.NYU.EDU>
2018-01-31 14:43:00 -0500

    public & /somethingelse

--- week2/server.js
+++ week2/server.js
@@ -1,12 +1,18 @@
 //app.use(express.static('public'));
 
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.send('HEY')
-})
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
+
+app.get('/somethingelse', function (req, res) {
+  res.send('AYE YO')
+});
