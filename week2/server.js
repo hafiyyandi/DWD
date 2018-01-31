4087d827320afffd475218f1dea45a91fafd6078
MOHAMMAD RIZQI HAFIYYANDI - <mohammadrizqihafiyyandi-@172-16-240-178.DYNAPOOL.NYU.EDU>
2018-01-31 15:11:41 -0500

    added dynamic form

--- week2/server.js
+++ week2/server.js
@@ -3,11 +3,27 @@ var app = express();
 
 var count=0;
 
+var submission = [];
+
 app.use(express.static('public'));
 
 app.get('/count', function (req, res) {
   count++;
   res.send("<html><body><h1>"+count+"</h1></body></html>");
+});
+
+app.get('/formpost', function (req, res){
+	submission.push(req.query.textfield);
+	res.redirect('/display');
+});
+
+app.get('/display', function(req, res){
+	var htmlout = "<html><body";
+	for (var i = 0; i<submission.length; i++){
+		htmlout = htmlout + submssions[i] + "<br>";
+	}
+	var htmlout = htmlout + "</body></html>";
+	res.send(htmlout);
 });
 
 app.get('/', function (req, res) {
