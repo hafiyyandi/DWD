cd170f77fd31163f90146d527654cd72a4e62995
MOHAMMAD RIZQI HAFIYYANDI - <mohammadrizqihafiyyandi-@172-16-243-112.DYNAPOOL.NYU.EDU>
2018-02-08 14:27:55 -0500

    moved port to 80

--- livestream/server.js
+++ livestream/server.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
+app.listen(80, function () {
   console.log('Example app listening on port 3000!')
 });
 
