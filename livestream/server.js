d0f3f7639aad026919668d182009a2f80140c929
MOHAMMAD RIZQI HAFIYYANDI - <mohammadrizqihafiyyandi-@172-16-243-112.DYNAPOOL.NYU.EDU>
2018-02-08 15:19:00 -0500

    debugging 7

--- livestream/server.js
+++ livestream/server.js
@@ -25,25 +25,32 @@ app.get('/', function (req, res) {
 });
 
 
-app.get('/fblogin', function (req, res) {
-	var loginURL = "https://www.facebook.com/v2.12/dialog/oauth?";
-	var app_id = "294044474454157";
-	var redirect_uri = "http://165.227.205.200:3000/loggedin"
-	var state_param = "st=state123"
-	//loginURL = loginURL + "client_id={" + app_id + "}&redirect_uri={" + redirect_uri + "}&state={" + state_param + "}"
-	loginURL = loginURL + "client_id=" + app_id + "&redirect_uri=" + redirect_uri + "&state={" + state_param + "}";
-	console.log(loginURL);
+// app.get('/fblogin', function (req, res) {
+// 	var loginURL = "https://www.facebook.com/v2.12/dialog/oauth?";
+// 	var app_id = "294044474454157";
+// 	var redirect_uri = "http://165.227.205.200:3000/loggedin"
+// 	var state_param = "st=state123"
+// 	//loginURL = loginURL + "client_id={" + app_id + "}&redirect_uri={" + redirect_uri + "}&state={" + state_param + "}"
+// 	loginURL = loginURL + "client_id=" + app_id + "&redirect_uri=" + redirect_uri + "&state={" + state_param + "}";
+// 	console.log(loginURL);
 	
-	res.redirect(loginURL);
-});
+// 	res.redirect(loginURL);
+// });
+
+// app.get('/loggedin', function (req,res){
+// 	res.send('HELLO you\'re logged in, I guess');
+
+// });
 
 app.get('/fbtest', function (req, res) {
 	res.render('template.ejs', data);
 });
 
-app.get('/loggedin', function (req,res){
-	res.send('HELLO you\'re logged in, I guess');
 
+app.get('/getlivestream', function (req, res) {
+	var token = req.query.token;
+	console.log(token);
+  	res.send('Hi there, your token is: ' token;
 });
 
 
