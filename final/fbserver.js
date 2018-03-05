71a765d35d98ed6af4d458d69f626db533f2564e
MOHAMMAD RIZQI HAFIYYANDI - <mohammadrizqihafiyyandi-@172-16-240-38.DYNAPOOL.NYU.EDU>
2018-03-04 19:27:42 -0500

    debugging 1

--- final/fbserver.js
+++ final/fbserver.js
@@ -63,7 +63,7 @@ app.get('/loggedin', function (req, res) {
 		
 		// Got the access token			
 		console.log("Access Token: " + facebookRes.access_token);
-		res.send(facebookRes);
+		console.log(facebookRes);
 		graph.setAccessToken(facebookRes.access_token);
 
 		// At this point it probably makes more sense to set the access token into a user session or the like so that the user doesn't have to authenticate every time and that we keep a different one for each user.
@@ -81,7 +81,7 @@ app.get('/loggedin', function (req, res) {
 			// 	vidIDs.push({id:current_id, status:current_status});
 			// }
 			//console.log(vidIDs);
-			//res.send(liveVids);
+			res.send(facebookRes);
 			//res.render('vidlist.ejs', {data:liveVids});
 			
 		});
